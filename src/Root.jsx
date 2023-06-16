26bd901013e2adbe8cdf46a1e6b64c83a67802fb
luisja.rodriguez <luisjaroma@gmail.com>
2023-06-16 12:16:07 +0200

    Corrección perqueño error

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,10 +1,8 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "./components/Header/Header";
 import { Footer } from "./components/Footer/Footer";
-import { LoginOrSignup } from "./Pages/LoginOrSignup/LoginOrSignup";
-import { Filter } from "./components/Filter/Filter";
 import { LoginOrSignup } from "./components/LoginOrSignup/LoginOrSignup";
-
+import { Filter } from "./components/Filter/Filter";
 
 export const Root = () => {
   return (
